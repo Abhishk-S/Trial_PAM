cd5c8b027e05aab501e809b3b4d83d6892818bbe
Abhishek <161257771+Abhishk-S@users.noreply.github.com>
2024-03-15 20:39:50 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-from setuptools import setup
+from distutils.core import setup
+from Cython.Build import cythonize
 
-with open("README.md","r") as fh:
-    long_description = fh.read()
-setup(
-    name='frgd',
-    version='0.0.1',
-    description='Functional Renormalization Group Solver for Multiband Hamiltonians',
-    packages=["frgd"],
-    install_requires=['numba','numpy','python_version>="3.5"'],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/nkyirga/frg2d",
-    author="Nahom Kifle Yirga",
-    author_email="nkyirga@bu.edu"
-)
+setup(name='auxFunctions',ext_modules=cythonize('auxFunctions.pyx'),)
+setup(name='interpP1F',ext_modules=cythonize('interpP1F.pyx'),)
